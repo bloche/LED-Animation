fa0d177ac67d3afa9ccb2e7d0911d26570f57959
Everett Bloch <Everett@Everetts-MacBook-Air.local>
2013-10-31 16:59:29 -0700

    Completed convert subroutine, and implemented prototype to test it

--- src/led_interface/convert.c
+++ src/led_interface/convert.c
@@ -5,25 +5,116 @@
    to a uint8_t** array.
 */
 
-#include "color.h"
+#include "convert.h"
 #include "stdlib.h"
 
-uint8_t** convert(color** in_matrix, int in_width, int in_hight,
-                  int out_width, int out_height,
-                  int wise, int scan) 
+/* converts in_matirx with in_width columns and in_height rows and returns 
+   a matrix three times larger with 3 rows and (in_width * in_hight) 
+   columns.
+
+   wise determines row (ROW_WISE) or column (COL_WISE) wise copy of in_matrix, 
+   and scan determines weather to scan from one side to another (SCAN_MONODIR)
+   , or back and forth (SCAN_BIDIR).
+*/
+uint8_t** convert(color** in_matrix, 
+		  unsigned int in_width, unsigned int in_height,
+                  unsigned int wise, unsigned int scan) 
 {
-  unint8_t** out_matrix;
-  int in_size = in_width * in_height;
-  int out_size = out_width * out_height;
+  uint8_t** out_matrix;
+  unsigned int in_size = in_width * in_height;
+  int pos = 0;
+  int i, j;
 
-  int i;
+  // allocate memory  
+  out_matrix = (uint8_t**) malloc(sizeof(uint8_t*) * 3);
+  for(i=0; i<3; i++)
+    out_matrix[i] = (uint8_t*) malloc(sizeof(uint8_t) * in_size);
 
-  // resulting matrix must be 3 times larger than in_matrix
-  if (out_size != 3 * in_size)
+  switch (scan) {
+  case SCAN_MONODIR:
+    switch (wise) {
+    case ROW_WISE:
+      // row wise copy
+      for(i=0; i<in_height; i++) 
+	for(j=0; j<in_width; j++) {
+	  out_matrix[0][pos] = in_matrix[i][j].R;
+	  out_matrix[1][pos] = in_matrix[i][j].G;
+	  out_matrix[2][pos] = in_matrix[i][j].B;
+	  pos++;
+	}
+      break;
+
+    case COL_WISE:
+      // column wise copy
+      for(i=0; i<in_width; i++) 
+	for(j=0; j<in_height; j++) {
+	  out_matrix[0][pos] = in_matrix[j][i].R;
+	  out_matrix[1][pos] = in_matrix[j][i].G;
+	  out_matrix[2][pos] = in_matrix[j][i].B;
+	  pos++;
+	}
+      break;
+
+    default:
+      return NULL;
+    } // end switch (wise)
+
+    break; // end SCAN_MONODIR
+
+  case SCAN_BIDIR:
+    switch (wise) {
+    case ROW_WISE:
+      // row wise copy
+      for(i=0; i<in_height; i++) {
+	for(j=0; j<in_width; j++) {
+	  out_matrix[0][pos] = in_matrix[i][j].R;
+	  out_matrix[1][pos] = in_matrix[i][j].G;
+	  out_matrix[2][pos] = in_matrix[i][j].B;
+	  pos++;
+	}
+	i++;
+	if(i<in_height) 
+	  // copy next row in other direction
+	  for(j=in_width-1; j>=0; j--) {
+	    out_matrix[0][pos] = in_matrix[i][j].R;
+	    out_matrix[1][pos] = in_matrix[i][j].G;
+	    out_matrix[2][pos] = in_matrix[i][j].B;
+	    pos++;
+	  }
+      }
+      break;
+      
+    case COL_WISE:
+      // column wise copy
+      for(i=0; i<in_width; i++) {
+	for(j=0; j<in_height; j++) {
+	  out_matrix[0][pos] = in_matrix[j][i].R;
+	  out_matrix[1][pos] = in_matrix[j][i].G;
+	  out_matrix[2][pos] = in_matrix[j][i].B;
+	  pos++;
+	}
+	i++;
+	if(i<in_width) 
+	  // copy next col in other direction
+	  for(j=in_height-1; j>=0; j--) { 
+	    out_matrix[0][pos] = in_matrix[j][i].R;
+	    out_matrix[1][pos] = in_matrix[j][i].G;
+	    out_matrix[2][pos] = in_matrix[j][i].B;
+	    pos++;
+	  }
+      }
+      break;
+    
+    default: // invalid wise value
+      return NULL;
+    } // end switch (wise)
+    
+    break; // end SCAN_BIDIR
+    
+  default:
     return NULL;
+    
+  } // end switch (scan)
   
-  out_matrix = (uint8_t**) malloc(sizeof(uint8_t*) * in_height);
-  for(i=0; i<in_height; i++) {
-    out_matrix[i] = (uint8_t*) malloc(sizeof(uint8_t) * in_width);
-  }
+  return out_matrix;
 }
--- src/led_interface/convert.h
+++ src/led_interface/convert.h
@@ -4,21 +4,25 @@
    This header file contains the declaration of the convert function.
    This function converts an arbitrary color** array to a matrix that is
    compatible with the LED display drivers created by team RPLD.
+   The enum is for valid values for wise and scan values for the 
+   convert function.
    The color structure is defined in color.h.
 */
 
 #include "color.h"
+#include <stdint.h>
+
+// enum used for wise and scan values
+enum {ROW_WISE, COL_WISE, SCAN_MONODIR, SCAN_BIDIR};
 
 /* convert in_matrix with in_width and in_height demintions, to a
-   matrix of out_width and out_height demintions.
+   matrix three times larger, 3x(in_width * in_height)
    
    wise determins row wise or column wise; scan determines single 
    deirection scan or back and forth scan of in_matrix.
 */
 uint8_t** convert(color** in_matrix, 
-		  int in_width, 
-		  int in_hight, 
-		  int out_width, 
-		  int out_height, 
-		  int wise, 
-		  int scan); 
+		  unsigned int in_width, 
+		  unsigned int in_hight, 
+		  unsigned int wise, 
+		  unsigned int scan); 
